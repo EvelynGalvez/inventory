6da9fe8c2c176392046dcab7e629314e0b9346cf
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-10-21 21:29:16 -0300

    index.blade

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,12 @@
-<?php
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            Productos
+        </h2>
+        <div class="flex flex-wrap px-8 -mx-2 overflow-hidden lg:px-0 sm:px-0 xl:px-0">
+            <div class="flex px-2 my-2 overflow-hidden">
+
+            </div>
+        </div>
+    </x-slot>
+</x-app-layout>
