661f03c46f7775ca7824b11902d5eea89e7c000b
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-10-23 01:05:41 -0300

    input readonly en show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -6,35 +6,35 @@
             <form action="/" method="post">
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="first_name">ID Producto</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="first_name" id="first_name" value="{{$id}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="first_name" id="first_name" value="{{$id}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Código Producto</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$codigo}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$codigo}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Nombre Producto</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$nombre}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$nombre}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Categoría</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$categoria}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$categoria}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Sucursal</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$sucursal}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$sucursal}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Descripción</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$descripcion}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$descripcion}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Cantidad</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$cantidad}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$cantidad}}" readonly>
                 </div>
                 <div class="flex flex-col mb-4">
                     <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Precio</label>
-                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$precio}}">
+                    <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" value="{{$precio}}" readonly>
                 </div>
 
             </form>
